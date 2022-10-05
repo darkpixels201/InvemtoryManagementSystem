62d5ad451c97ebac8dee69b2561739990645bf45
darkpixels201 <darkpixels201@gmail.com>
2022-10-05 12:06:56 +0500

    ModalChnages

--- src/components/customComponents/ViewModal.js
+++ src/components/customComponents/ViewModal.js
@@ -1,18 +1,42 @@
-import { CButton, CModalBody, CModalFooter } from '@coreui/react'
-import React from 'react'
+import {
+  CButton,
+  CCardImage,
+  CModalBody,
+  CModalFooter,
+  CModalTitle,
+} from "@coreui/react";
+import React from "react";
+import { Image } from "react-bootstrap";
+// import pratice from "../../assets/images/pratice";
+import avatar6 from "../../assets/images/avatars/6.jpg";
+// import avatar6 from "../../assets/images/avatars/6.jpg";
 
-function ViewModal({setEyeVisible}) {
+function ViewModal({ setEyeVisible, tableExample }) {
   return (
     <div>
-    <CModalBody>View View Content</CModalBody>
- <CModalFooter>
-   <CButton color="secondary" onClick={() => setEyeVisible(false)}>
-     Close
-   </CButton>
-   <CButton color="primary">Save changes</CButton>
- </CModalFooter>
-</div>
-  )
+      <CModalBody className="d-flex flex-wrap">
+        <div className=" img-fluid col-sm-6 align-self-center mx-auto d-block  ">
+        <Image className="img-fluid" src={avatar6} rounded />
+        </div>
+
+        <div className="d-flex flex-wrap col-sm-6 align-items-left flex-column ">
+          <CModalTitle>Product Name</CModalTitle>
+          <CModalBody>nfjdlvafdnv djkfnkjdsbkjdbskb kdsbfbdsjkkdv bfkhbasdbfck asbkfasbkb  mavsfjhbsk,dskhj</CModalBody>
+          <CModalTitle>Sale Price</CModalTitle>
+          <CModalTitle>Purchase Price</CModalTitle>
+          <CModalTitle>Stock</CModalTitle>
+          <CModalTitle>Status</CModalTitle>
+        </div>
+      </CModalBody>
+      <CModalFooter>
+        <CButton color="secondary" onClick={() => setEyeVisible(false)}>
+          Close
+        </CButton>
+        <CButton color="primary">Save changes</CButton>
+      </CModalFooter>
+    </div>
+  );
 }
 
-export default ViewModal
+export default ViewModal;
+
